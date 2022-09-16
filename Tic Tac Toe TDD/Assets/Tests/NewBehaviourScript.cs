ff1f0e86b3d0055a5fa115133a0801fc6806a838
Joan Ginard Mateo <jgm7.hd@gmail.com>
2022-09-16 22:32:13 +0200

    Tests setup

---
+++ Tic Tac Toe TDD/Assets/Tests/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // TDD: Tests -> Code
+    // TFD: Code -> Tests
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
