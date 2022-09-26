0b14edbe6a76f120cf29a60115c7ca8ed8992324
Joan Ginard Mateo <jgm7.hd@gmail.com>
2022-09-26 11:11:39 +0200

    rows result tests

--- Assets/Tests/RowsResultTest.cs
+++ Assets/Tests/RowsResultTest.cs
@@ -2,7 +2,6 @@
 using JGM.Model;
 using Moq;
 using NUnit.Framework;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -24,12 +23,60 @@ namespace JGM.Tests
             boardControllerMock = new Mock<BoardController>();
             boardControllerMock.Setup(mock => mock.Rows).Returns(defaultBoardRows);
             boardControllerMock.Setup(mock => mock.Columns).Returns(defaultBoardColumns);
+            boardControllerMock.Object.GameIsPlaying = true;
         }
 
+        [TestCase(0f,
+                  0, 0, 0,
+                  1, -1, 1,
+                  1, -1, -1)]
+        [TestCase(0f,
+                 -1, 1, 1,
+                  0, 0, 0,
+                  1, -1, -1)]
+        [TestCase(0f,
+                 -1, 1, 1,
+                  1, -1, -1,
+                  0, 0, 0)]
+        [TestCase(1f,
+                  1, 1, 1,
+                  0, -1, 0,
+                  0, -1, -1)]
+        [TestCase(1f,
+                 -1, 0, 0,
+                  1, 1, 1,
+                  0, -1, -1)]
+        [TestCase(1f,
+                 -1, 0, 0,
+                  0, -1, -1,
+                  1, 1, 1)]
+        public void When_CalculatingWinnerResult_Expect_GameIsPlayingAsFalse(float winner, params int[] gridValues)
+        {
+            FillBoardWithValues(gridValues);
+            rowsResult.Calculate(boardControllerMock.Object);
+            boardControllerMock.Verify(mock => mock.GetCell(It.IsAny<Vector2Int>()), Times.AtLeast(defaultBoardRows));
+            LogAssert.Expect(LogType.Log, $"Game Over - Winner is Player {(int)winner}!");
+            Assert.IsFalse(boardControllerMock.Object.GameIsPlaying);
+        }
+
+        [TestCase(1, 0, 0,
+                  1, -1, 1,
+                  0, -1, -1)]
+        [TestCase(0, 1, 1,
+                  0, 0, 1,
+                 -1, -1, -1)]
         [TestCase(-1, 0, 0,
-                   1, 1, 1,
-                   0, -1, -1)]
-        public void When_CalculatingWinnerResult_Expect_Log(params int[] gridValues)
+                   0, 1, 1,
+                  -1, -1, -1)]
+        public void When_CalculatingNoWinnerResult_Expect_GameIsPlayingAsTrue(params int[] gridValues)
+        {
+            FillBoardWithValues(gridValues);
+            rowsResult.Calculate(boardControllerMock.Object);
+            boardControllerMock.Verify(mock => mock.GetCell(It.IsAny<Vector2Int>()), Times.Exactly(defaultBoardRows * defaultBoardColumns));
+            Assert.IsTrue(boardControllerMock.Object.GameIsPlaying);
+        }
+
+        private void FillBoardWithValues(int[] gridValues)
         {
             var boardModel = new BoardModel(defaultBoardRows, defaultBoardColumns);
             int counter = 0;
@@ -40,15 +87,7 @@ namespace JGM.Tests
                     boardModel.SetCell(new Vector2Int(i, j), gridValues[counter++]);
                 }
             }
-            boardControllerMock.Setup(mock => mock.GetCell(It.IsAny<Vector2Int>())).Returns(boardModel.GetCell(It.IsAny<Vector2Int>()));
-            rowsResult.Calculate(new Mock<BoardController>().Object);
-            LogAssert.Expect(LogType.Log, $"Game Over - Winner is Player {1}!");
-        }
-
-        [Test]
-        public void When_CalculatingNoWinnerResult_Expect_NextChainMethodIsCalled()
-        {
-            //TODO continue tomorrow
+            boardControllerMock.Setup(mock => mock.GetCell(It.IsAny<Vector2Int>())).Returns((Vector2Int coordinates) => boardModel.GetCell(coordinates));
         }
     }
 }
